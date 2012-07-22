f9b9a1f74fea082bdaebf6b4f0a9eb9c75e509de
Viet Coop <contact@vietcoop.com>
2012-07-22 12:06:05 +0700

    Initial commit

---
+++ tools/memcache/Config/Memcache.php
@@ -0,0 +1,28 @@
+<?php
+return array (
+  'stats_api' => 'Server',
+  'slabs_api' => 'Server',
+  'items_api' => 'Server',
+  'get_api' => 'Server',
+  'set_api' => 'Server',
+  'delete_api' => 'Server',
+  'flush_all_api' => 'Server',
+  'connection_timeout' => '1',
+  'max_item_dump' => '100',
+  'refresh_rate' => 5,
+  'memory_alert' => '80',
+  'hit_rate_alert' => '90',
+  'eviction_alert' => '0',
+  'file_path' => 'Temp/',
+  'servers' =>
+  array (
+    'Default' => array (
+      'tunnel.pagodabox.com:11211' => array ('hostname' => 'tunnel.pagodabox.com', 'port' => '11211'),
+      'tunnel.pagodabox.com:11212' => array ('hostname' => 'tunnel.pagodabox.com', 'port' => '11212'),
+      'tunnel.pagodabox.com:11213' => array ('hostname' => 'tunnel.pagodabox.com', 'port' => '11213'),
+      'tunnel.pagodabox.com:11214' => array ('hostname' => 'tunnel.pagodabox.com', 'port' => '11214'),
+      'tunnel.pagodabox.com:11215' => array ('hostname' => 'tunnel.pagodabox.com', 'port' => '11215'),
+      'tunnel.pagodabox.com:11216' => array ('hostname' => 'tunnel.pagodabox.com', 'port' => '11216'),
+    ),
+  ),
+);
